5370c0e3b442bdef07365677bc66bf6df634ea5d
joran <joran_02@163.com>
2019-10-12 17:17:57 +0800

    增加测试用例

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
+#include<unistd.h>
 #include "timer.h"
+
+void cbfunc(){
+
+	printf("cb function\n");
+
+}
+
 
 int main(){
 
 	timer_init();
 
-	timer_deinit();
-	return 0;
+	int nTimerId = timer_start(-1, 1000, "my_timer",
+	                                 (timer_cb)cbfunc, NULL);
+	while(1){
+		usleep(100);
+	}
 }
